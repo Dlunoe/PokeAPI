cc92b313f6fbdf3e4bdb156c5dfcaf794d392870
Dustin Lunoe <dustinlunoe@Dustins-MacBook-Pro.local>
2019-06-28 13:04:45 -0600

    Added search list and can pull up data through said list. Styled buttons on list.

--- app.js
+++ app.js
@@ -9,8 +9,63 @@ $('.searchButton').on("click",function(){
         $.ajax({
         url:`https://pokeapi.co/api/v2/type/${searchFor}`,
         success:(data)=>{
-            console.log(data)
+            //appends search results as buttons
+            for(i=0;i<20;i++){
             
+            $('.searchResults').append(`<li><input type="button" class="search" value="${data.pokemon[i].pokemon.name}"></input></li>`)
+            }
+            $('.searchResults').append('<input type="button" value="Next Page" class="nextButton"></input>')
+            $('.nextButton').on("click", function(e){
+               //clicking next will bring up the next 20 results
+                for(i=i;i<i+20;i++){                   
+                    $('.searchResults').append(`<li><input type="button" class="search" value="${data.pokemon[i].pokemon.name}"></input></li>`)
+                    }
+            })
+            //same code as seaching for a name, but uses the button to input a name instead of a search box.
+            $('.search').on("click", function(e){
+                event.preventDefault();
+                searchFor=(this.value);
+                        $.ajax({
+                        url:`https://pokeapi.co/api/v2/pokemon/${searchFor}`,
+                        success:(data)=>{
+                            console.log(data);
+                            var sprite=data.sprites.front_default;
+                            var spriteBack=data.sprites.back_default;
+                            $('.name').text(data.species.name)
+                            $('.sprite').html(`<img src=${sprite}>`)
+                            $('.sprite2').html(`<img src=${spriteBack}>`)
+                            $('.types').empty();
+                            $('.types').append(`<li>${data.types[0].type.name}</li>`)
+                                if(data.types[1].type.name!==undefined){
+                                $('.types').append(`<li>${data.types[1].type.name}</li>`)
+                                }
+                
+                        },
+                        error:()=>{
+                            console.log("bad request")
+                        }
+                
+                        })
+                        $.ajax({
+                        url:`https://pokeapi.co/api/v2/pokemon-species/${searchFor}`,
+                        success:(data)=>{
+                            console.log(data)
+                            var flavorText = "";
+                            for(i=0;i<data.flavor_text_entries.length;i++){
+                                if(data.flavor_text_entries[i].language.name=="en"){
+                                    flavorText=data.flavor_text_entries[i].flavor_text;
+                                    $('.flavorText').text(flavorText);
+                                    return true
+                                }
+                            }
+                        },
+                        error:()=>{
+                            console.log("bad request")
+                        }
+                        })
+                
+
+            })
         },
         error:()=>{
             console.log("bad request")
@@ -19,7 +74,6 @@ $('.searchButton').on("click",function(){
     
     //if the user searches a name
     }else{
-
     $.ajax({
         url:`https://pokeapi.co/api/v2/pokemon/${searchFor}`,
         success:(data)=>{
@@ -59,6 +113,7 @@ $('.searchButton').on("click",function(){
         }
     })
 }
+    
     })
 
 
